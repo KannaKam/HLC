86445f731d2beb5b40e3c091796b0c8e439c6b2b
Kanna <dynibmons@gmail.com>
2020-10-15 08:40:02 +0200

    Ejercicio intro Angular

---
+++ 03introAngular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { SugerenciasComponent } from './pages/sugerencias/sugerencias.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    NoticiasComponent,
+    SugerenciasComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
